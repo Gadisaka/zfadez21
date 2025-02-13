5398fc461b0846744338ab0e8a34a6fe5a1832ba
gadisa kasaye <gadisaka04@gmail.com>
2025-02-13 22:05:13 +0300

    Add shop items display with images and layout adjustments

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,9 +1,35 @@
+import Image from "next/image";
 import React from "react";
+import item1 from "@/public/images/klara-kulikova-0jOBJSU6tow-unsplash (1).jpg";
+import item2 from "@/public/images/klara-kulikova-0jOBJSU6tow-unsplash (1).jpg";
+
+const items = [
+  {
+    id: 1,
+    name: "item1",
+    price: 100,
+    image: item1,
+  },
+  {
+    id: 2,
+    name: "item2",
+    price: 200,
+    image: item2,
+  },
+];
 
 const Shop = () => {
   return (
     <div className="w-full min-h-fit flex flex-col bg-[#F8E1B7] text-[#543310] gap-10 lg:p-10 justify-center items-center ">
       Shop
+      {items.map((item) => (
+        <div
+          key={item.id}
+          className=" h-[300px] w-[300px] flex flex-col justify-center items-center bg-[#F8E1B7] text-[#543310] gap-10 lg:p-10 "
+        >
+          <Image src={item.image} alt={item.name} />
+        </div>
+      ))}
     </div>
   );
 };
